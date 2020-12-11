4f252280212f33e3b3e0f2b00b5890a3787f9888
Galdric PUJOL <55428835+onfire25@users.noreply.github.com>
2020-12-11 10:53:21 +0100

    Add files via upload

--- demineur.js
+++ demineur.js
@@ -1,6 +1,8 @@
 
 var grille;
 var caseJeu = 5;
+//var imgFont = document.createElement("img");
+var imgFont = "Image/empty.png";
 
 function createcase() {
     grille = new Array(9);
@@ -8,10 +10,10 @@ function createcase() {
         {
         	grille[i]= new Array(9);
         	for (var x = 0; x < 9; x++) {
-            	grille[i][x]= caseJeu;
+            	grille[i][x]= imgFont;
         	}
     	}
-    $('div#grille').text(grille);
+    $('div#grille').text (grille);
 }
 
 
