2f781f00260fd66bdbb12161efce85f9c76a35fc
Galdric PUJOL <55428835+onfire25@users.noreply.github.com>
2020-12-11 11:34:23 +0100

    Add files via upload
    Bombe ok

--- demineur.js
+++ demineur.js
@@ -1,23 +1,50 @@
-
 var grille;
-var caseJeu = 5;
+var casex = 9;
+var casey = 9;
+var caseJeu = 0;
+var bombe = "b";
+var nbBombe = 10;
+var nbrCase = 9;
 //var imgFont = document.createElement("img");
-var imgFont = "Image/empty.png";
+//imgFont.src = "Image/empty.png";
 
 function createcase() {
-    grille = new Array(9);
-    for (var i = 0; i < 9; i++)
+    grille = new Array(casex);
+    for (var i = 0; i < casex; i++)
         {
-        	grille[i]= new Array(9);
-        	for (var x = 0; x < 9; x++) {
-            	grille[i][x]= imgFont;
+        	grille[i]= new Array(casey);
+        	for (var a = 0; a < casey; a++) {
+            	grille[i][a]= caseJeu;
         	}
-    	}
-    $('div#grille').text (grille);
+        }
+    //$('div#grille').text(grille);
+}
+
+function ajoutBombe() {
+    for (var i = 0; i < nbBombe; i++)
+    {
+        var x = getRandomInt(casex);
+        var y = getRandomInt(casey);
+        grille[x][y] = bombe;
+    }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
 
 
 
+function afficherGrille(grille, nbrCase){
+    //$('div#grille').text(grille);
+    for (var i = 0; i < nbrCase; i++){
+        $('div#grille').append(grille[i] + '</br>');
+    }
+}
+
 $(document).ready(function() {
+    console.log('ok');
     createcase();
+    ajoutBombe();
+    afficherGrille(grille, nbrCase);
 })
