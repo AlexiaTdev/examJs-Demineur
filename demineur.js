5ad0bc7d4d105b6cdd9ca28d8bc9dc45d6aba18c
AlexiaTdev <alexia.teissier1@epsi.fr>
2020-12-11 10:47:11 +0100

    k

--- demineur.js
+++ demineur.js
@@ -1,20 +1,28 @@
-
 var grille;
 var caseJeu = 5;
+var nbrCase = 9;
 
 function createcase() {
     grille = new Array(9);
     for (var i = 0; i < 9; i++)
         {
-        for (var x = 0; x < 9; x++) {
-            grille[i][x]= caseJeu;
+        	grille[i]= new Array(9);
+        	for (var x = 0; x < 9; x++) {
+            	grille[i][x]= caseJeu;
+        	}
         }
-    }
-    $('div#grille').text(grille);
+    //$('div#grille').text(grille);
+    afficherGrille(grille, nbrCase);
 }
 
-
+function afficherGrille(grille, nbrCase){
+    //$('div#grille').text(grille);
+    for (var i = 0; i < nbrCase; i++){
+        $('div#grille').append(grille[i] + '</br>');
+    }
+}
 
 $(document).ready(function() {
+    console.log('ok');
     createcase();
 })
