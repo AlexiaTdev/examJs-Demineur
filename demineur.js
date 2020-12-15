803fdc6b36dcc341bddc3ad546af3a202fcdb45e
AlexiaTdev <alexia.teissier1@epsi.fr>
2020-12-15 18:00:13 +0100

    avec les click droits

--- demineur.js
+++ demineur.js
@@ -1,18 +1,18 @@
+var boolfinDuJeu = false;
 var grille;
 var grilleRevellee;
 //var caseVerifieeRecursivite;
 
-var nbCasex = 9;
-var nbCasey = 9;
-
-
-var nbrBomb = 10;
+var nbCasex;
+var nbCasey;
+var nbrBomb;
 
 var nbBombeAlentour = 0;
 
 
 var listEtat = ["images/empty.png", "images/1.png", "images/2.png", "images/3.png", "images/4.png", "images/5.png", "images/6.png", "images/7.png", "images/8.png", "images/normal.png", "images/bomb.png", "images/flag.png" ]
-
+var listeNbrCases = [9, 16, 22, 30];
+var listeNbrBombes = [10, 40, 100, 250];
 var pathArray = "file://" + window.location.pathname.slice(0, -10);
 
 //var listeNumeros = [listEtat[1], listEtat[2], listEtat[3], listEtat[4], listEtat[5], listEtat[6], listEtat[7], listEtat[8]];
@@ -30,10 +30,12 @@ function createcase(nbCasex, nbCasey) {
         grilleRevellee[i]=[];
         for (var x = 0; x < nbCasex; x++) {
             var imgFont= document.createElement('img');
-            imgFont.dataset.src = listEtat[9];
+            ///ATTENTION NE FONCTIONNE PAS AVEC DATASET SRC
+            //imgFont.dataset.src = listEtat[9];
             imgFont.dataset.xr = x;
             imgFont.dataset.yr = i;
             grilleRevellee[i][x]= imgFont;
+            grilleRevellee[i][x].src = pathArray+listEtat[9];
         }
     }
 
@@ -48,7 +50,7 @@ function ajoutBombe() {
     {
         var x = getRandomInt(nbCasex);
         var y = getRandomInt(nbCasey);
-        grille[y][x]= listEtat[10];
+        grille[y][x] = listEtat[10];
     }
 }
 function renseigneLeNbrBombe(){
@@ -138,6 +140,7 @@ function jouerLaCaseRecursif(coordX, coordY){
 
         // si on tombe sur une case vide (sans numéro ni bombe)
         if (grille[coordY][coordX] == listEtat[0]){
+            grilleRevellee[coordY][coordX].src = pathArray+listEtat[0];
             if (coordX+1<=nbCasex-1){
                 jouerLaCaseRecursif(Number(coordX)+1, Number(coordY));
                 if (0<=coordY-1){
@@ -164,8 +167,6 @@ function jouerLaCaseRecursif(coordX, coordY){
             if (0<=coordY-1){
                 jouerLaCaseRecursif(Number(coordX), Number(coordY)-1);
             }
-            grilleRevellee[coordY][coordX].src = pathArray+listEtat[0];
-
         }
         // si on tombe sur une case avec un chiffre
         if (listEtat.includes(grille[coordY][coordX])){
@@ -180,36 +181,72 @@ function jouerLaCaseRecursif(coordX, coordY){
 
 
 function finDuJeu(valeurBoolFin){
+    boolfinDuJeu = true;
     //gagné
     if (valeurBoolFin){
-
-    } 
+        $('div#resultat').text("vous avez gagne");
+    }
     //perdu
     else {
         for (var i = 0; i < nbCasey; i++) {
         	for (var x = 0; x < nbCasex; x++) {
-                if (grille[i][x].src ==  pathArray+listEtat[10]){
+                if (grille[i][x] ==  listEtat[10]){
                     grilleRevellee[i][x].src = pathArray+listEtat[10];
                 }
             }
         }
+        $('div#resultat').text("vous avez perdu");
     }
     afficherGrille();
 }
 
+function playflag(x, y) {
+    grilleRevellee[y][x].src = pathArray+listEtat[11];
+    afficherGrille()
+}
+
+function initialiserJeu() {
+    boolfinDuJeu = false;
+    $('div#grille').text("");
+    $('div#resultat').text("");
+
+    nbCasex = listeNbrCases[document.getElementById('typeGrille').value];
+    nbCasey = nbCasex;
+
+    nbrBomb = listeNbrBombes[document.getElementById('typeGrille').value];
+}
+
 
 function newStart(){
-    $('div#grille').text("");
+    initialiserJeu();
+    
+
     createcase(nbCasex, nbCasey);
     ajoutBombe();
     renseigneLeNbrBombe();
     afficherGrille();
-
-    $('img').on('click', function() {
-        playcase(this.dataset.xr, this.dataset.yr);});
+    console.log(document.getElementById('typeGrille').value)
 }
 
 $(document).ready(function() {
     $('#debutpartie').on('click', newStart);
 })
+
+$(document).on('click', function() {
+    if (!boolfinDuJeu){
+        $('img').on('click', function() {playcase(this.dataset.xr, this.dataset.yr);});
+    }
+    noContext = document.getElementById('grille');
+            noContext.addEventListener('contextmenu', e => {
+                e.preventDefault();
+            });
+})
+$(document).on('contextmenu', function() {
+    if (!boolfinDuJeu){
+        $('img').on('contextmenu', function() {
+            playflag(this.dataset.xr, this.dataset.yr)
+        });
+    }
+})
+
 
